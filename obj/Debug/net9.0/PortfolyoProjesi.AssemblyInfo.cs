ae3dbd0fc262c7b66dc33b563111acdb42a9879b
kemalkaratoprak <kemalxkaratoprak@gmail.com>
2026-05-24 00:07:54 +0300

    Google Search Console doğrulaması eklendi

--- obj/Debug/net9.0/PortfolyoProjesi.AssemblyInfo.cs
+++ obj/Debug/net9.0/PortfolyoProjesi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolyoProjesi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f024c6ade1f3cb4d1860bfc729e7bacb13a16ad2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad5f44d55d9e2ed9bc8e2f76651f341924214141")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolyoProjesi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolyoProjesi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
